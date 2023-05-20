0fc899fa4cc0a13e4a1cbf6a9525a29624e81bb6
alphadigital1 <tecnologia@alphalumen.org.br>
2023-05-19 21:05:17 -0300

    update TCPDF

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '6c1e4901fc14b7385df345a9f41f994dcb2d761f',
+        'reference' => 'd89e578eb5112a05eb6bb039a698d44c3ebe3d9d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '6c1e4901fc14b7385df345a9f41f994dcb2d761f',
+            'reference' => 'd89e578eb5112a05eb6bb039a698d44c3ebe3d9d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
